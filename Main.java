093840d47264a758e1abece12313d0f502b43237
Mishka3645 <131558613+Mishka3645@users.noreply.github.com>
2023-05-17 21:14:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        for(int i = 0; i < a + 1; ++i)
+        {
+            if(i == 0)
+                System.out.print(i + " ");
+            else if(i % 3 == 0 && i % 5 == 0)
+                System.out.print("fizzbuzz" + " ");
+            else if(i % 3 == 0)
+                System.out.print("fizz" + " ");
+            else if(i % 5 == 0)
+                System.out.print("buzz" + " ");
+            else
+                System.out.print(i + " ");
+        }
+    }
+}
